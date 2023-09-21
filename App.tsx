2aaae33e43703e581175dbeb4092c0cf86eb558e
erik-ferreira <erikdesouzagf@gmail.com>
2023-09-21 20:50:48 -0300

    add necessary deps; create animation with hold menu

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import { StatusBar } from 'react-native';
+import "react-native-gesture-handler"
 
-import { Home } from './src/screens/Home';
+import { StatusBar } from "react-native"
+import { Feather } from "@expo/vector-icons"
+import { HoldMenuProvider } from "react-native-hold-menu"
+
+import { Home } from "./src/screens/Home"
 
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    <HoldMenuProvider
+      theme="dark"
+      iconComponent={Feather}
+      safeAreaInsets={{ bottom: 0, left: 0, right: 0, top: 0 }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Home />
-    </>
-  );
+    </HoldMenuProvider>
+  )
 }
--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,16 +1,35 @@
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, View } from "react-native"
+import Animated, { FadeIn, FadeOut, Layout } from "react-native-reanimated"
+import { HoldItem } from "react-native-hold-menu"
 
-import { styles } from './styles';
+import { styles } from "./styles"
 
 type Props = {
-  title: string;
-  onRemove: () => void;
-};
+  title: string
+  onRemove: () => void
+}
 
 export function Tag({ title, onRemove }: Props) {
   return (
-    <TouchableOpacity  style={styles.container} onPress={onRemove}>
-      <Text style={styles.title}>#{title}</Text>
-    </TouchableOpacity>
-  );
+    <Animated.View
+      style={styles.container}
+      layout={Layout}
+      entering={FadeIn}
+      exiting={FadeOut}
+    >
+      <HoldItem
+        items={[
+          { text: title, isTitle: true },
+          {
+            text: "Apagar",
+            isDestructive: true,
+            onPress: onRemove,
+            icon: "trash",
+          },
+        ]}
+      >
+        <Text style={styles.title}>#{title}</Text>
+      </HoldItem>
+    </Animated.View>
+  )
 }
